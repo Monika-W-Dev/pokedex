03476db4ddd7b6c28a5034ea34c3b13f60f27321
Monika We <mail.mw.dev@web.de>
2026-03-11 15:23:57 +0100

    adding function fetchData

--- script.js
+++ script.js
@@ -1,33 +1,42 @@
-let pokemonsData = [
-    { 
-        'id': 1,
-        'name': 'bulbasaur',
-        'img': 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg',
-        'alt': 'bulbasaur',
-        'types': [
-            'grass', 'poison'
-        ],
-        'height': 170,
-        'weight': 70,
-        'abilities': ['blaze', 'solar'],
-        'base_stats': {
-            'hp': 45,
-            'attack': 65,
-            'defense': 48,
-            'special-attack': 22,
-            'special-defense': 30,
-            'speed': 45
-        },
-        'evolution': [ 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg', 
-            'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/2.svg', 
-            'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/3.svg' 
-        ]
-    }
-];
+let pokemonsData = [];
+
+const BASE_URL = "https://pokeapi.co/api/v2/"
+const NUMBER_PER_LOAD = 30;
 
 function init() {
-    renderPokemonOverview();
+    fetchData();
 }
+
+async function fetchData() {
+    try {
+        let answer = await fetch(BASE_URL + "pokemon/1");
+        let data = await answer.json();
+        console.log(data);
+       await getDataOfPokemon(data);
+       await renderPokemonOverview();
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+function getDataOfPokemon(data) {
+    const newPokemon = {
+        'id': data.id,
+        'name': data.name,
+        'img': data.sprites.other.dream_world.front_default,
+        'alt': data.name,
+        'types': data.types.map(t => t.type.name),
+        'height': data.height,
+        'weight': data.weight,
+        'abilities': data.abilities.map(a => a.ability.name),
+        'base_stats': data.stats.reduce((acc, s) => {
+            acc[s.stat.name] = s.base_stat;
+            return acc;
+        }, {})
+    };
+    pokemonsData.push(newPokemon);
+}
+
 
 function renderPokemonOverview() {
     let overviewSection = document.getElementById('list_view');
@@ -40,14 +49,14 @@ function renderPokemonOverview() {
 function renderTypes(pokemonIndex) {
     let renderTypesHtml = "";
     for (let typesIndex = 0; typesIndex < pokemonsData[pokemonIndex].types.length; typesIndex++) {
-      renderTypesHtml += pokemonTypesTemplate(pokemonIndex, typesIndex);  
+        renderTypesHtml += pokemonTypesTemplate(pokemonIndex, typesIndex);
     }
     return renderTypesHtml;
 }
 
 function renderPokemonAbilities(pokemonIndex) {
     return pokemonsData[pokemonIndex].abilities.map(ability => {
-       return `${ability.charAt(0).toUpperCase() + ability.slice(1)}`;
+        return `${ability.charAt(0).toUpperCase() + ability.slice(1)}`;
     }).join(', ');
 }
 
@@ -56,6 +65,3 @@ function capitalLetters(name) {
     return capitalName;
 }
 
-function fetchData() {
-
-}
